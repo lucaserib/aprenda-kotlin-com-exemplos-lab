14da3f7e151854b73553ea2a0a8ffb0464d6cbb1
lucaserib <137096507+lucaserib@users.noreply.github.com>
2023-10-18 07:24:51 -0300

    Otimização de niveis de usuário e também atualização em forma de retorno

--- desafio.kt
+++ desafio.kt
@@ -8,41 +8,41 @@ class Usuario(
 
 data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
-
+class Formacao(
+    val nome: String,
+    val conteudos: List<ConteudoEducacional>,
+    val tipo: String // Adicionamos um tipo para a formação
+) {
     var inscritos = mutableListOf<Usuario>()
-    
+
     fun matricular(usuario: Usuario) {
         inscritos.add(usuario)
-        println("O Usuário ${usuario.nome} foi matriculado na formação $nome com nível ${usuario.nivel}.")
-    }
-}
-
-fun calcularNivel(duracaoTotal: Int): Nivel {
-    return when {
-        duracaoTotal <= 100 -> Nivel.BASICO
-        duracaoTotal <= 160 -> Nivel.INTERMEDIARIO
-        else -> Nivel.AVANCADO
+        // Define o nível com base no tipo da formação
+        usuario.nivel = when (tipo) {
+            "Programing Basics" -> Nivel.BASICO
+            "Kotlin Introduction" -> Nivel.INTERMEDIARIO
+            "Kotlin Development" -> Nivel.AVANCADO
+            else -> null
+        }
+        println("O Usuário ${usuario.nome}, de ${usuario.idade} anos foi matriculado na formação $nome com tipo $tipo e nível ${usuario.nivel}.")
     }
 }
 
 fun main() {
     val conteudo1 = ConteudoEducacional("Programing Basics", 100)
     val conteudo2 = ConteudoEducacional("Kotlin Introduction", 160)
-    val conteudo3 = ConteudoEducacional("Kotlin Developement", 220)
+    val conteudo3 = ConteudoEducacional("Kotlin Development", 220)
     
-    val formacao = Formacao("Kotlin Development", listOf(conteudo1, conteudo2, conteudo3))
+    val formacao1 = Formacao("Formação 1", listOf(conteudo1, conteudo2, conteudo3), "Programing Basics")
+    val formacao2 = Formacao("Formação 2", listOf(conteudo1, conteudo2, conteudo3), "Kotlin Introduction")
+    val formacao3 = Formacao("Formação 3", listOf(conteudo1, conteudo2, conteudo3), "Kotlin Development")
     
     val usuario1 = Usuario("Lucas", 22)
     val usuario2 = Usuario("Luana", 24)
     val usuario3 = Usuario("Pedro", 23)
 
-    val duracaoTotal = formacao.conteudos.sumBy { it.duracao }
-    usuario1.nivel = calcularNivel(duracaoTotal)
-    usuario2.nivel = calcularNivel(duracaoTotal)
-    usuario3.nivel = calcularNivel(duracaoTotal)
-    
-    formacao.matricular(usuario1)
-    formacao.matricular(usuario2)
-    formacao.matricular(usuario3)
+    // Matricular os alunos em diferentes formações
+    formacao1.matricular(usuario1)
+    formacao2.matricular(usuario2)
+    formacao3.matricular(usuario3)
 }
